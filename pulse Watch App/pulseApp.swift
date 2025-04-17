877a5ec4ab688cd7f1679091669f1437be5befad
lismin <lixumin1030@gmail.com>
2025-04-17 20:27:51 +0800

    Initial Commit

---
+++ pulse Watch App/pulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  pulseApp.swift
+//  pulse Watch App
+//
+//  Created by lixumin on 2025/4/17.
+//
+
+import SwiftUI
+
+@main
+struct pulse_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pulse/pulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  pulseApp.swift
+//  pulse
+//
+//  Created by lixumin on 2025/4/17.
+//
+
+import SwiftUI
+
+@main
+struct pulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
